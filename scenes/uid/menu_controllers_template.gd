f4348148382c7acc85fe4662aae9525136339542
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-06 21:47:27 +0200

    Initial commit

---
+++ scenes/uid/menu_controllers_template.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var Button0 = $Buttons/Button0
+@onready var Button1 = $Buttons/Button1
+@onready var Button2 = $Buttons/Button2
+
+func _on_button_0_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.

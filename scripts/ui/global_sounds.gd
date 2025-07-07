89b0e14437679537e1acce0683a23080f75e9514
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-07 16:00:17 +0200

    Fix sound cut-off
    Creado archivo global de reproducción de sonidos

---
+++ scripts/ui/global_sounds.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var audio_player: AudioStreamPlayer
+
+func _ready():
+	audio_player = AudioStreamPlayer.new()
+	add_child(audio_player)
+	audio_player.bus = "Music"
+
+func play_sound(path: String):
+	if not FileAccess.file_exists(path):
+		push_error("Archivo de sonido no encontrado: " + path)
+		return false
+	
+	var stream = load(path)
+	if stream is AudioStream:
+		audio_player.stream = stream
+		audio_player.play()
+		return true
+	else:
+		push_error("No se pudo cargar el archivo de audio: " + path)
+		return false
--- scripts/ui/menu_controllers_template.gd
+++ scripts/ui/menu_controllers_template.gd
@@ -1,25 +1,24 @@
-### ARCHIVO PLANTILLA"
-
 extends Control
+
+const sound = "res://assets/audio/sfx/Jam_Player_Tambor_1_Sonido.ogg"
 
 @onready var Button0 = $Buttons/Button0
 @onready var Button1 = $Buttons/Button1
 @onready var Button2 = $Buttons/Button2
-@onready var button_sound = $Buttons/ButtonSound
 
-func _on_button_0_button_down() -> void:	
+func _on_button_0_button_down() -> void:
 	print("Button 0 pressed")
-	button_sound.play()
-	await get_tree().create_timer(0.1).timeout
+	if not GlobalSounds.play_sound(sound):
+		print("Error: no se pudo reproducir el sonido")
 
 
 func _on_button_1_button_down() -> void:
 	print("Button 1 pressed")
-	button_sound.play()
-	await get_tree().create_timer(0.1).timeout
+	if not GlobalSounds.play_sound(sound):
+		print("Error: no se pudo reproducir el sonido")
 
 
 func _on_button_2_button_down() -> void:
-	print("Button 2 pressed")
-	button_sound.play()
-	await get_tree().create_timer(0.1).timeout
+	print("Button 1 pressed")
+	if not GlobalSounds.play_sound(sound):
+		print("Error: no se pudo reproducir el sonido")

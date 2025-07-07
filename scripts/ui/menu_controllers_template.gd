27ac8d66347258f42ae9a7a362ca1e29fb85e814
ElMagoRubio <yanoleomicorreo@gmail.com>
2025-07-07 07:05:42 +0200

    sonido botones

--- scripts/ui/menu_controllers_template.gd
+++ scripts/ui/menu_controllers_template.gd
@@ -1,23 +1,26 @@
-extends Control
+### ARCHIVO PLANTILLA"
 
-signal button_0_pressed
-signal button_1_pressed
-signal button_2_pressed
+extends Control
 
 @onready var Button0 = $Buttons/Button0
 @onready var Button1 = $Buttons/Button1
 @onready var Button2 = $Buttons/Button2
+@onready var button_sound = $Buttons/ButtonSound
 
-func _on_button_0_pressed() -> void:
-	print("Button 0")
-	emit_signal("button_0_pressed")
+func _on_button_0_pressed():
+	print("Button 0 pressed")
+	button_sound.play()
+	await get_tree().create_timer(0.1).timeout
+	
 
 
-func _on_button_1_pressed() -> void:
+func _on_button_1_pressed():
 	print("Button 1 pressed")
-	emit_signal("button_1_pressed")
+	button_sound.play()
+	await get_tree().create_timer(0.1).timeout
 
 
-func _on_button_2_pressed() -> void:
+func _on_button_2_pressed():
 	print("Button 2 pressed")
-	emit_signal("button_2_pressed")
+	button_sound.play()
+	await get_tree().create_timer(0.1).timeout
